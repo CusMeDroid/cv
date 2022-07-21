c9cb4279bbff0c6ceda64a80fd3cdf8e1fdaa9b5
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-22 01:17:37 +0700

    Add files via upload

---
+++ mohjakayulianto/jquery/cv-jQuery.js
@@ -0,0 +1,22 @@
+function submitFunc() {
+    var name = document.getElementById('name');
+    var phone = document.getElementById('phone');
+    var subject = document.getElementById('subject');
+    if (name.value < 1 || phone.value < 1 || subject.value < 1) {
+        alert('Please fill out this field!');
+    } else {
+        window.open('mailto:mohammadjakayulianto@gmail.com?subject='+name.value+', '+phone.value+'&body='+subject.value);
+    }
+}
+
+if ('mediaSession' in navigator) {
+    navigator.mediaSession.metadata = new MediaMetadata({
+        artist : 'Denny Caknan.mp3',
+        title: 'SATRU 2.mp3',
+        artwork: [{
+            src: 'https://assets.pikiran-rakyat.com/crop/0x0:0x0/750x500/photo/2022/07/01/1152666922.jpg',
+            sizes: '320x180',
+            type: 'image/png'
+        }]
+    })
+}
