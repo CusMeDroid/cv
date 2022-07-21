82696b9624be0100d1d37ae4f7ba52494648bdc5
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-21 17:26:30 +0700

    Add files via upload

---
+++ sahrul/jquery/cv-jQuery.js
@@ -0,0 +1,22 @@
+function submitFunc() {
+    var name = document.getElementById('name');
+    var phone = document.getElementById('phone');
+    var subject = document.getElementById('subject');
+    if (name.value < 1 || phone.value < 1 || subject.value < 1) {
+        alert('Please fill out this field!');
+    } else {
+        window.open('mailto:sahrulti12@gmail.com?subject='+name.value+', '+phone.value+'&body='+subject.value);
+    }
+}
+
+if ('mediaSession' in navigator) {
+    navigator.mediaSession.metadata = new MediaMetadata({
+        artist : 'Maroon 5',
+        title: 'Memories',
+        artwork: [{
+            src: 'https://1.bp.blogspot.com/-rIK1fSxr8Lw/Xb9PWSeAe2I/AAAAAAAAIfA/BU7xtpm35Jg3HCmmaIbD1RUMODHS2DgpACLcBGAsYHQ/s640/Memories.jpg',
+            sizes: '320x180',
+            type: 'image/png'
+        }]
+    })
+}
