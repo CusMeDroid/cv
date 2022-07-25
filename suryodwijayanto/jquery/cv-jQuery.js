8d0db2f88e4444d26606deaa86191b99e754cef7
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-25 10:25:47 +0700

    Update cv-jQuery.js

--- suryodwijayanto/jquery/cv-jQuery.js
+++ suryodwijayanto/jquery/cv-jQuery.js
@@ -24,3 +24,25 @@ if ('mediaSession' in navigator) {
         }]
     })
 }
+
+/* Birthday */
+var date = '07/09/1994'.value;
+ 
+if(date === ""){
+    alert("Please complete the required field!");
+}else{
+    var today = new Date();
+    var birthday = new Date(date);
+    var year = 0;
+    if (today.getMonth() < birthday.getMonth()) {
+        year = 1;
+    } else if ((today.getMonth() == birthday.getMonth()) && today.getDate() < birthday.getDate()) {
+        year = 1;
+    }
+    var age = today.getFullYear() - birthday.getFullYear() - year;
+
+    if(age < 0){
+        age = 0;
+    }
+    document.getElementById('myold').innerHTML = age;
+}
